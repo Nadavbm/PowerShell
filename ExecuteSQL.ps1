ce298bcf7d70112b172194635d89b7df4000c401
Nadav Ben Mazia <nadav.benmazia@aeriagames.com>
2019-01-16 17:37:04 +0100

    Services restart and SQL edition

--- ExecuteSQL.ps1
+++ ExecuteSQL.ps1
@@ -10,8 +10,19 @@ $dir = "E:\Path\To\SQL\Queries"
 # Create array from list of files
 $arr = @(Get-ChildItem -Path $dir -Filter *.sql -Recurse -File -Name)
 
+Write-Host "#################################" -ForegroundColor Cyan
+Write-Host "        List SQL Files           " -ForegroundColor Cyan
+Write-Host "#################################" -ForegroundColor Cyan
+echo $arr
+
+sleep 10
+
 # SQL Instance
 $serverInstance = "10.10.10.10\Instance"
+
+Write-Host "#################################" -ForegroundColor Cyan
+Write-Host "       Execute SQL Files         " -ForegroundColor Cyan
+Write-Host "#################################" -ForegroundColor Cyan
 
 # Running the loop to execute each query from remote
 foreach ($file in $arr) {
